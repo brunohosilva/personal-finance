9f3353e7f7a013712914dac614444031afe33aa3
Bruno Oliveira <bruno.osilva@neon.com.br>
2023-04-30 17:05:01 -0300

    :tada: first commit config react native project

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import AppScreen from './src';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AppScreen);
